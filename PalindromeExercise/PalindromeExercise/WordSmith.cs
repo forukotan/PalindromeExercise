b41c4aa7940617873bb1fcfc390c00c704990ec0
forukotan <forukotan1@gmail.com>
2023-03-09 13:39:12 -0600

    all the test passed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            // what is a palindrome
+            // reverse of the word is euqalto the original word
+            // return true or false 
+            
+            
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string reversed = new string(charArray);
+               if (reversed == word)
+                { 
+                    return true; 
+                }
+               else
+                {
+                    return false;
+                }
+            
+        }
+         
+    }
+}
